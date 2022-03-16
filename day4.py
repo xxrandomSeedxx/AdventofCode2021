4f7fd84f4dacccef05b5e5f54a23c9cf7915825f
lam <shun_min@hotmail.com>
2022-03-16 17:49:38 +0800

    - fix logic - add checkVertical function

--- day4.py
+++ day4.py
@@ -2,33 +2,87 @@
 import sys
 
 input = sys.stdin.readlines()
-draw_num = input[0]
-toCheck = list()
-print(draw_num)
+draw_num = input[0].split(",")
 
-def checkHorizontal(puzzle):
-    for i in range(0, len(puzzle)):
-        print(">>>>", puzzle[i])
+
+def checkHorizontal(toCheck, puzzleLines):
+    for i in range(0, len(puzzleLines)):
+        line = puzzleLines[i]
+        count = 0
+        for digit in toCheck:
+            if digit not in line:
+                break
+            else:
+                count += 1
+        print("Horz Line {} match count: {}".format(i, count))
+        if count == 5:
+            return True
+        else:
+            continue
+    return False
+
+
+def checkVertical(toCheck, puzzleLines):
+    for i in range(0, 5):
+        vertLine = list()
+        count = 0
+        for j in range(0, len(puzzleLines)):
+            vertLine.append(puzzleLines[j][i])
+
+        for digit in toCheck:
+            if digit not in vertLine:
+                break
+            else:
+                count += 1
+        print("Vert ine {} match count: {}".format(i, count))
+        if count == 5:
+            return True
+        else:
+            continue
+    return False
+
+
+def checkDiagonal():
+
+
 
 def checkBoard(toCheck, boardToCheck):
-    checkHorizontal(toCheck, boardToCheck)
-    #checkVertical()
-    #checkDiagonal()
+    gotMatch = None
+    gotMatch = checkHorizontal(toCheck, boardToCheck)
+    if not gotMatch:
+        gotMatch = checkVertical(toCheck, boardToCheck)
+    if not gotMatch:
+        gotMatch = checkDiagonal(toCheck, boardToCheck)
+    else:
+        print("Match >>>>>>>>>>>>>>")
 
-def getBoard(startLine, puzzleList):
+
+def getBoard(puzzleStartLine, puzzleList):
     boardToCheck = list()
-    for i in range(startLine, startLine + 5):
-        boardToCheck.append(puzzleList[i])
+    for i in range(puzzleStartLine, puzzleStartLine + 5):
+        try:
+            boardToCheck.append(puzzleList[i])
+        except IndexError:
+            pass
     return boardToCheck
 
 puzzleList = input[2:]
 i = 0
-startLine = 0
+puzzleStartLine = 0
+toCheck = list()
+
 while i < len(draw_num):
-    if i < 5:
+    print("Draw: ", draw_num[i])
+    if i == 0 or i % 5 != 0:
         toCheck.append(draw_num[i])
-        continue
+        i += 1
     else:
-        boardToCheck = getBoard(startLine, puzzleList)
+        print("Start line: ", puzzleStartLine)
+        boardToCheck = getBoard(puzzleStartLine, puzzleList)
+        print("Digits to check:", toCheck)
+        print("Board to check: \n", boardToCheck)
         checkBoard(toCheck, boardToCheck)
-    i += 1
+        puzzleStartLine += 6
+
+        toCheck = [draw_num[i]]  # reset digits to check after every five lines
+        i += 1
