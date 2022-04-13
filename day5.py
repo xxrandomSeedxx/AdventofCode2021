884485005bf54f6d3cc82ff329f6fd89f3a15d30
xxrandomSeedxx <shun_min@hotmail.com>
2022-04-13 21:45:52 +0800

    - store full coods

--- day5.py
+++ day5.py
@@ -3,15 +3,26 @@ import sys
 
 input = sys.stdin.readlines()
 
+
+def store_full_coods(start, end, value):
+    line_full_cood = list()
+    start = start[value]
+    end = end[value]
+    print(">>>>", start, end)
+
 for num_grp in input:
+    all_coods = list()
     try:
         coods = num_grp.strip("\n").split(" -> ")
-        set1 = coods[0]
-        set2 = coods[1]
-        # print(set1, set2)
-        if set1[0] == set2[0]:
-            print(set1[0], set2[0], "x is same")
-        elif set1[2] == set2[2]:
-            print(set1[2], set2[2], "y is same")
+        start = coods[0]  # 0,9
+        end = coods[1]  # 5,9
+        # compare x
+        if start[0] == end[0]:
+            print("x coods are same")
+            store_full_coods(start, end, 2)
+        # compare y (set[1] is ",")
+        elif start[2] == end[2]:
+            print("y coods are same")
+            store_full_coods(start, end, 0)
     except IndexError:
         pass
