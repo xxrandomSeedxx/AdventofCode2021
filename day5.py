7dc611ec369c5317882cb3bfdc9e4208b97fe79c
xxrandomSeedxx <shun_min@hotmail.com>
2022-06-01 23:05:11 +0800

    - collect all plotted points

--- day5.py
+++ day5.py
@@ -3,26 +3,32 @@ import sys
 
 input = sys.stdin.readlines()
 
-def store_full_coods(start, end, value):
-    line_full_cood = list()
-    start = start[value]
-    end = end[value]
-    print(">>>>", start, end)
 
+class Counter(object):
+    # def __init__(self):
+    #     super().__init__()
+
+    def plot_points(self, constant, start, end):
+        count = 1
+        points = dict()
+        for i in range(int(start), int(end) + 1):
+            count += 1
+        print(self.plotted_points)
+
+all_points = list()
 for num_grp in input:
-    all_coods = list()
     try:
-        cood_grp = num_grp.strip("\n").split(" -> ")
-        start = cood_grp[0].split(",")  # ['432,708', '432,160'] -> [[432,708], [432,160]]
-        end = cood_grp[1].split(",")
-        print(start, end)
-        # compare x
-        if start[0] == end[0]:
-            print("x coods are same")
-            store_full_coods(start, end, 1)
-        # compare y
-        elif start[1] == end[1]:
-            print("y coods are same")
-            store_full_coods(start, end, 0)
+        cood_grp = num_grp.strip("\n").split(" -> ")  # ['432,708', '432,160']
+        x1, y1 = cood_grp[0].split(",")
+        x2, y2 = cood_grp[1].split(",")
+
+        if x1 == x2 or y1 == y2:
+            for y in range(int(min(y1, y2)), int(max(y1, y2)) + 1):
+                print(">>", x1, y)
+                all_points.append((x1, y))
+            for x in range(int(min(x1, x2)), int(max(x1, x2)) + 1):
+                print("++", x, y1)
+                all_points.append((x, y1))
     except IndexError:
         pass
+# print(all_points)
